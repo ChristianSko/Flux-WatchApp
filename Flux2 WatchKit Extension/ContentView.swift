bcad4b472c4113c4fcf22dd7312883c40fa28d4b
Christian Skorobogatow <christiansko@icloud.com>
2020-11-27 10:35:03 +0100

    changed to 6 buttons

--- Flux2 WatchKit Extension/ContentView.swift
+++ Flux2 WatchKit Extension/ContentView.swift
@@ -9,38 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        NavigationLink(destination: TimerView()) {
-            VStack {
-                Group{
-                    Text("")
-                        .fontWeight(.light)
-                        .font(.footnote)
-                        .padding(.bottom, -7.0)
-                    
-                    Text("25")
-                        .font(.system(size: 30, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.blue)
-                        .multilineTextAlignment(.center)
-                        .lineLimit(1)
-                        .frame(width: 85, height: 30, alignment: .center)
-                    
-                    Text("MIN")
-                        .fontWeight(.light)
-                        .foregroundColor(Color.white)
-                        .padding(.top, -10.0)
-                    
+        ScrollView{
+            VStack{
+                ForEach(0..<3) {number in
+            HStack{
+                ForEach(0..<2) { number in
+                    NavigationLink(destination: TimerView()) {
+                        VStack {
+                            Group{
+                                Text("")
+                                    .fontWeight(.light)
+                                    .font(.footnote)
+                                    .padding(.bottom, -7.0)
+                                
+                                Text("25")
+                                    .font(.system(size: 30, design: .rounded))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.blue)
+                                    .multilineTextAlignment(.center)
+                                    .lineLimit(1)
+                                    .frame(width: 85, height: 30, alignment: .center)
+                                
+                                Text("MIN")
+                                    .fontWeight(.light)
+                                    .foregroundColor(Color.white)
+                                    .padding(.top, -10.0)
+                                
+                            }
+                        }
+                    }
+                    .background(Color.black)
+                    .frame(width: 68, height: 68, alignment: .center)
+                    .cornerRadius(50)
+                    .overlay(Circle()
+                                .strokeBorder(style: StrokeStyle(lineWidth: 4,dash: [2])))
+                    .padding()
+                }
+            }
                 }
             }
         }
-        .background(Color.black)
-        .frame(width: 68, height: 68, alignment: .center)
-        .cornerRadius(50)
-        .overlay(Circle()
-                    .strokeBorder(style: StrokeStyle(lineWidth: 4,dash: [2])))
-        .padding()
-        
     }
 }
 
