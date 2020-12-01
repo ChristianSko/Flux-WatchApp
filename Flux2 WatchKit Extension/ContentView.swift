1d90e6fcf57d8733be076a5b28c39be0a0110f50
Christian Skorobogatow <christiansko@icloud.com>
2020-12-01 10:13:47 +0100

    SessionView set up for code maintainability

--- Flux2 WatchKit Extension/ContentView.swift
+++ Flux2 WatchKit Extension/ContentView.swift
@@ -8,44 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sessionMinutes = 25
+    @State var cycles: Double = 2
+    
+    let sessionTypes = [25, 45]
+    
     var body: some View {
         ScrollView{
-            VStack{
-                ForEach(0..<3) {number in
             HStack{
-                ForEach(0..<2) { number in
-                    NavigationLink(destination: TimerView()) {
-                        VStack {
-                            Group{
-                                Text("")
-                                    .fontWeight(.light)
-                                    .font(.footnote)
-                                    .padding(.bottom, -7.0)
-                                
-                                Text("25")
-                                    .font(.system(size: 30, design: .rounded))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.blue)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(1)
-                                    .frame(width: 85, height: 30, alignment: .center)
-                                
-                                Text("MIN")
-                                    .fontWeight(.light)
-                                    .foregroundColor(Color.white)
-                                    .padding(.top, -10.0)
-                                
+                ForEach(0..<2) {sessions in
+                    VStack{
+                        ForEach(0..<3) { number in
+                            NavigationLink(destination: TimerView(text: "\(number)")) {
+                                VStack {
+                                    Group{
+                                        Text("\(Int(pow(cycles, Double(number))))x")
+                                            .fontWeight(.light)
+                                            .font(.footnote)
+                                            .padding(.bottom, -7.0)
+                                        
+                                        Text("\(sessionTypes[sessions])")
+                                            .font(.system(size: 30, design: .rounded))
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.blue)
+                                            .multilineTextAlignment(.center)
+                                            .lineLimit(1)
+                                            .frame(width: 85, height: 30, alignment: .center)
+                                        
+                                        Text("MIN")
+                                            .fontWeight(.light)
+                                            .foregroundColor(Color.white)
+                                            .padding(.top, -13.0)
+                                    }
+                                }
                             }
+                            .background(Color.black)
+                            .frame(width: 68, height: 68, alignment: .center)
+                            .cornerRadius(50)
+                            .overlay(Circle()
+                            .strokeBorder(style: StrokeStyle(lineWidth: 4,dash: [2])))
+                            .padding()
                         }
                     }
-                    .background(Color.black)
-                    .frame(width: 68, height: 68, alignment: .center)
-                    .cornerRadius(50)
-                    .overlay(Circle()
-                                .strokeBorder(style: StrokeStyle(lineWidth: 4,dash: [2])))
-                    .padding()
-                }
-            }
                 }
             }
         }
