28e7f5b1fd72385eaa15210a6732c14ee4478047
Christian Skorobogatow <christiansko@icloud.com>
2021-05-28 23:37:46 +0200

    Adding Empty Weekly Summary View

---
+++ Flux2 WatchKit Extension/Screens/WeeklySummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  WeeklySummaryView.swift
+//  Flux2 WatchKit Extension
+//
+//  Created by Christian Skorobogatow on 28/5/21.
+//
+
+import SwiftUI
+
+struct WeeklySummaryView: View {
+    var body: some View {
+        Text("Weekly Summary View Goes her")
+    }
+}
+
+struct WeeklySummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeeklySummaryView()
+    }
+}
